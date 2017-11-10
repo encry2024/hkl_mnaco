7db67cbcab432e0a78d41f9e88a7609312a3cbcd
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2017-11-10 17:02:03 +0800

    Added functions: create customer view, store customer.

--- app/Http/Controllers/Backend/Management/Customer/CustomerController.php
+++ app/Http/Controllers/Backend/Management/Customer/CustomerController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Management\Customer\Customer;
 use App\Repositories\Backend\Management\Customer\CustomerRepository;
-use App\Request\Backend\Management\Customer\ManageCustomerRequest;
+use App\Http\Requests\Backend\Management\Customer\ManageCustomerRequest;
+use App\Http\Requests\Backend\Management\Customer\StoreCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -16,80 +17,93 @@ class CustomerController extends Controller
    public function __construct(CustomerRepository $customers)
    {
       $this->customers = $customers;
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('backend.customer.index');
-    }
+   }
+   /**
+    * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+   public function index(ManageCustomerRequest $request)
+   {
+      return view('backend.customer.index');
+   }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   /**
+    * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+   public function create()
+   {
+      return view('backend.customer.create');
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+   /**
+    * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+   public function store(StoreCustomerRequest $request)
+   {
+      $this->customers->create(
+      [
+         'data' => $request->only(
+            'company_name',
+            'contact_person',
+            'contact_number',
+            'alternative_number',
+            'address',
+            'notes',
+            'email'
+         ),
+      ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+      return redirect()->route('admin.management.customer.index')->withFlashSuccess(trans('alerts.backend.management.customer.created'));
+   }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+   /**
+    * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+   public function show($id)
+   {
+      //
+   }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+   /**
+    * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+   public function edit($id)
+   {
+      //
+   }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+   /**
+    * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+   public function update(Request $request, $id)
+   {
+      //
+   }
+
+   /**
+    * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+   public function destroy($id)
+   {
+      //
+   }
 }
