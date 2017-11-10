79e2791bc9fbc0389b8fd7844f5c76f3e554ef3e
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2017-11-10 16:08:43 +0800

    Added index for customer module

--- resources/views/backend/customer/index.blade.php
+++ resources/views/backend/customer/index.blade.php
@@ -1,6 +1,6 @@
 @extends ('backend.layouts.app')
 
-@section ('title', trans('labels.backend.customer.management'))
+@section ('title', trans('labels.backend.management.inventory.customer.title'))
 
 @section('after-styles')
 {{ Html::style("https://cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.css") }}
@@ -8,18 +8,18 @@
 
 @section('page-header')
 <h1>
-   {{ trans('labels.backend.customer.management') }}
-   <small>{{ trans('labels.backend.customer.availables') }}</small>
+   {{ trans('labels.backend.management.inventory.customer.title') }}
+   <small>{{ trans('labels.backend.management.inventory.customer.availables') }}</small>
 </h1>
 @endsection
 
 @section('content')
 <div class="box box-success">
    <div class="box-header with-border">
-      <h3 class="box-title">{{ trans('labels.backend.customer.availables') }}</h3>
+      <h3 class="box-title">{{ trans('labels.backend.management.inventory.customer.availables') }}</h3>
 
       <div class="box-tools pull-right">
-
+         @include('backend.customer.includes.partials.customer-header-buttons')
       </div><!--box-tools pull-right-->
    </div><!-- /.box-header -->
 
@@ -28,12 +28,13 @@
          <table id="customers-table" class="table table-condensed table-hover">
             <thead>
                <tr>
-                  <th>id</th>
-                  <th>company name</th>
-                  <th>contact number</th>
-                  <th>created at</th>
-                  <th>last updated</th>
-                  <th>actions</th>
+                  <th>{{ trans('labels.backend.management.inventory.customer.table.id') }}</th>
+                  <th>{{ trans('labels.backend.management.inventory.customer.table.company_name') }}</th>
+                  <th>{{ trans('labels.backend.management.inventory.customer.table.contact_number') }}</th>
+                  <th>{{ trans('labels.backend.management.inventory.customer.table.contact_person') }}</th>
+                  <th>{{ trans('labels.backend.management.inventory.customer.table.created_at') }}</th>
+                  <th>{{ trans('labels.backend.management.inventory.customer.table.updated_at') }}</th>
+                  <th>{{ trans('labels.general.actions') }}</th>
                </tr>
             </thead>
          </table>
@@ -61,23 +62,23 @@
 <script>
 $(function () {
    $('#customers-table').DataTable({
-       dom: 'lfrtip',
+      dom: 'lfrtip',
       processing: true,
       serverSide: true,
-       autoWidth: false,
+      autoWidth: false,
       ajax: {
          url: '{{ route("admin.management.customer.get") }}',
          type: 'post',
          data: {trashed: false}
       },
       columns: [
-         {data: 'id', name: '{{config('customer.customers_table')}}.id'},
-         {data: 'company_name', name: '{{config('customer.customers_table')}}.company_name'},
-         {data: 'contact_person', name: '{{config('customer.customers_table')}}.contact_person'},
-         {data: 'contact_number', name: '{{config('customer.customers_table')}}.contact_number'},
-         {data: 'created_at', name: '{{config('customer.customers_table')}}.created_at'},
-         {data: 'updated_at', name: '{{config('customer.customers_table')}}.updated_at'},
-         {data: 'actions', name: 'actions', searchable: false, sortable: false}
+         {data: 'id',               name: '{{config('customer.customers_table')}}.id'},
+         {data: 'company_name',     name: '{{config('customer.customers_table')}}.company_name'},
+         {data: 'contact_person',   name: '{{config('customer.customers_table')}}.contact_person'},
+         {data: 'contact_number',   name: '{{config('customer.customers_table')}}.contact_number'},
+         {data: 'created_at',       name: '{{config('customer.customers_table')}}.created_at'},
+         {data: 'updated_at',       name: '{{config('customer.customers_table')}}.updated_at'},
+         {data: 'actions',          name: 'actions', searchable: false, sortable: false}
       ],
       order: [[0, "asc"]],
       searchDelay: 500
