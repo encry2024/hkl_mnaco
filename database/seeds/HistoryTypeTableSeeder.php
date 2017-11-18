fc5f86247f860466e96a5a2c3a5a7c32d7776364
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2017-11-18 16:00:32 +0800

    Added Liquidation History Seeder.

--- database/seeds/HistoryTypeTableSeeder.php
+++ database/seeds/HistoryTypeTableSeeder.php
@@ -44,6 +44,11 @@ class HistoryTypeTableSeeder extends Seeder
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
          ],
+         [
+            'name'       => 'Liquidation',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+         ],
       ];
 
       DB::table('history_types')->insert($types);
